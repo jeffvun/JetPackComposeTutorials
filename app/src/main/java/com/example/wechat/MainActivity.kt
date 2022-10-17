20e44f41b9a36390a87f3d0f2898d8035082f02b
jeffvun <vunangajoseph@gmail.com>
2022-10-18 02:15:12 +0300

    Material design update

---
+++ app/src/main/java/com/example/wechat/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.wechat
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.wechat.ui.theme.WeChatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeChatTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all =4.dp),
+                    style = MaterialTheme.typography.caption
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageReplyCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all =4.dp),
+                    style = MaterialTheme.typography.caption
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    WeChatTheme {
+        Surface {
+            MessageCard(
+                msg =  Message("Elie","Hey there...Take a look at Jetpack Compose")
+            )
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun DefaultPreview() {
+    WeChatTheme {
+        Surface {
+            MessageReplyCard(
+                msg =  Message("Othniel","Hey there...Take a look at Jetpack Compose")
+            )
+        }
+    }
+}
